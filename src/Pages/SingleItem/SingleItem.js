55d5e67c9b31aa41d5586c575759b6ea47438f0b
Arif <iariful950@gmail.com>
2022-05-05 00:28:20 +0600

    operation of delivered button done Alhamdulillah!

--- src/Pages/SingleItem/SingleItem.js
+++ src/Pages/SingleItem/SingleItem.js
@@ -4,19 +4,31 @@ import './SingleItem.css';
 
 const SingleItem = () => {
     const { id } = useParams();
-    // const url = `http://localhost:5000/items/${id}`;
-    // console.log(url);
-    // const _id = props.params;
     const [item, setItem] = useState({});
     useEffect(() => {
         fetch(`http://localhost:5000/items/${id}`)
             .then(res => res.json())
             .then(data => setItem(data));
-    }, []);
+    }, [item]);
 
     const updateQuantity = id => {
-
-        
+        let quantity = item.quantity;
+        // console.log(quantity);
+        quantity -= 1;
+        // console.log(quantity);
+        const updatedDetail = { quantity };
+        // console.log(updatedDetail);
+        fetch(`http://localhost:5000/items/${id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(updatedDetail),
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
     }
     // console.log(item);
 
@@ -30,7 +42,7 @@ const SingleItem = () => {
                         <h1 className='text-xl font-semibold text-indigo-600 pb-2'>{item.name}</h1>
                         <p className='text-gray-700 pb-1'> {item.description} </p>
                         <p className='text-xl font-medium text-indigo-500 pb-1'>Price: <span className='text-amber-400 font-semibold'>{item.price}</span></p>
-                        <h2 className='text-gray-600 text-xl font-bold pb-1 absolute -top-8 md:top-2 right-0 pr-0 md:pr-4'>Quantity: <span className='text-amber-400'>{item.quantity}</span></h2>
+                        <h2 className='text-gray-600 text-2xl font-bold pb-1 absolute -top-8 md:top-2 right-0 pr-0 md:pr-4'>Quantity: <span className='text-amber-400'>{item.quantity}</span></h2>
                         <h2 className='text-gray-700 pb-1'>Supplied from {item.supplier}</h2>
                         <h2 className='text-gray-700 pb-2'>Sold status No</h2>
                         <button onClick={() => updateQuantity(id)} className='w-32 bg-gray-800 py-2 font-medium text-white border-2 border-gray-700 rounded-sm hover:bg-stone-200 hover:text-black'>
