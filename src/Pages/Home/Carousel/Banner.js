2f3521ac1918a3e16b91dfdbf38fcceae5c35334
Arif <iariful950@gmail.com>
2022-05-05 02:01:29 +0600

    item restocked!

--- src/Pages/Home/Carousel/Banner.js
+++ src/Pages/Home/Carousel/Banner.js
@@ -10,7 +10,7 @@ import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
-        <div>
+        <div className='relative'>
             <Carousel autoPlay infiniteLoop interval={5000} showThumbs={false}>
                 <div>
                     <img className='w-full h-[450px] md:h-[450px] object-cover' src={img1} />
@@ -25,7 +25,20 @@ const Banner = () => {
                     <img className='w-full h-[450px] md:h-[450px] object-cover' src={img4} />
                 </div>
             </Carousel>
-            
+            <div className='flex justify-center'>
+                <div className="absolute bottom-28">
+                    <button className='hover:cursor-text font-normal text-xl text-white border-2 w-80 mx-auto border-white py-1'>
+                        Premium Sports Gear Available
+                    </button>
+                </div>
+            </div>
+            <div className="flex justify-center">
+                <div className='absolute bottom-14'>
+                    <button className='font-medium text-xl text-black bg-white border-2 w-60 py-1 mx-auto border-white hover:text-white hover:bg-gray-900 hover:border-gray-900 uppercase'>
+                        <Link to='/'>See Inventory</Link>
+                    </button>
+                </div>
+            </div>
         </div >
     );
 };
--- src/Pages/SingleItem/SingleItem.js
+++ src/Pages/SingleItem/SingleItem.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import './SingleItem.css';
 
 const SingleItem = () => {
+    const [input, setInput] = useState('');
     const { id } = useParams();
     const [item, setItem] = useState({});
     useEffect(() => {
@@ -11,7 +12,7 @@ const SingleItem = () => {
             .then(data => setItem(data));
     }, [item]);
 
-    const updateQuantity = id => {
+    const decreaseQuantity = id => {
         let quantity = item.quantity;
         // console.log(quantity);
         quantity -= 1;
@@ -31,29 +32,63 @@ const SingleItem = () => {
             })
     }
     // console.log(item);
+    let num;
+    // const getValue = event => {
+    //     num = event.target.value;
+    //     console.log('num', num);
+    // }
+
+    const increaseQuantity = (id) => {
+        let quantity = item.quantity;
+        num = parseInt(input);
+        console.log(input);
+        quantity += num;
+        quantity = parseInt(quantity);
+        console.log(quantity);
+        const updatedDetail = { quantity };
+        console.log(updatedDetail);
+        fetch(`http://localhost:5000/items/${id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(updatedDetail),
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+            })
+    }
 
     return (
-        <div className='bg-gray-100 pb-10'>
+        <div className='bg-gray-200 pb-10 2xl:pt-20 h-screen'>
             <div className='w-3/4 mx-auto pt-10'>
                 <div className='flex items-center justify-center flex-col xl:flex-row gap-14 md:gap-10'>
-                    <img className='w-[450px] rounded-sm' src={item.image} alt="" />
-                    <div className='bg-white p-4 relative'>
-                        <p className='pb-1'>You accessed the item with id <br /> <span className='text-gray-600'>{id}</span></p>
+                    <div>
+                        <img className='w-[450px] rounded-sm hover:opacity-75' src={item.image} alt="" />
+                    </div>
+                    <div className='bg-white p-4 relative rounded-sm'>
+                        <p className='pb-1'>You accessed the item with id <br /> <span className='text-gray-600 underline'>{id}</span></p>
                         <h1 className='text-xl font-semibold text-indigo-600 pb-2'>{item.name}</h1>
                         <p className='text-gray-700 pb-1'> {item.description} </p>
-                        <p className='text-xl font-medium text-indigo-500 pb-1'>Price: <span className='text-amber-400 font-semibold'>{item.price}</span></p>
+                        <p className='text-xl font-medium text-indigo-500 pb-1'>Price: <span className='text-amber-400 font-semibold '>{item.price}</span></p>
                         <h2 className='text-gray-600 text-2xl font-bold pb-1 absolute -top-8 md:top-2 right-0 pr-0 md:pr-4'>Quantity: <span className='text-amber-400'>{item.quantity}</span></h2>
                         <h2 className='text-gray-700 pb-1'>Supplied from {item.supplier}</h2>
-                        <h2 className='text-gray-700 pb-2'>Sold status No</h2>
-                        <button onClick={() => updateQuantity(id)} className='w-32 bg-gray-800 py-2 font-medium text-white border-2 border-gray-700 rounded-sm hover:bg-stone-200 hover:text-black'>
+                        <h2 className='text-gray-700 pb-2'>Sold status: No</h2>
+                        <button onClick={() => decreaseQuantity(id)} className='w-32 block bg-gray-800 py-2 mb-3 font-medium text-white border-2 border-gray-700 rounded-sm hover:bg-stone-200 hover:text-black'>
                             Delivered
                         </button>
+                        <div className='flex flex-col md:flex-row space-x-0 md:space-x-1 space-y-2 md:space-y-0 mb-1'>
+                            <input value={input} onInput={e => setInput(e.target.value)} className='w-72 pl-2 py-2 border-2 rounded-sm border-gray-700' type="text" name="text" id="" placeholder='Add quantity to restock the item' />
+                            <button onClick={() => increaseQuantity(id)} className='w-32 block bg-gray-800 py-2 font-medium text-white border-2 border-gray-700 rounded-sm hover:bg-stone-200 hover:text-black'>Add to stock</button>
+                        </div>
                     </div>
                 </div>
 
             </div>
 
         </div>
+
     );
 };
 
